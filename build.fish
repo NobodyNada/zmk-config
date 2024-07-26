c42308d84d192df7ff58d0aee1d0b0edb25516a0
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2024-07-26 15:09:41 -0700

    more gui keys

--- build.fish
+++ build.fish
@@ -1,2 +1,5 @@
 #!/usr/bin/env fish
-for side in left right; GNUARMEMB_TOOLCHAIN_PATH=/opt/homebrew ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb west build -p always -s zmk/app -b nice_nano_v2 -- -DZMK_CONFIG=(pwd)/config -DSHIELD=cradio_$side && cp build/zephyr/zmk.uf2 cradio_$side.uf2; end
+
+[ (uname -r) = Darwin ] && export GNUARMEMB_TOOLCHAIN_PATH=/opt/homebrew || export GNUARMEMB_TOOLCHAIN_PATH=/
+
+for side in left right; ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb west build -p always -s zmk/app -b nice_nano_v2 -- -DCMAKE_PREFIX_PATH=(pwd)/zephyr/share/zephyr-package/cmake -DZMK_CONFIG=(pwd)/config -DSHIELD=cradio_$side && cp build/zephyr/zmk.uf2 cradio_$side.uf2; end
