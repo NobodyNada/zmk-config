1d4a87f785ca0b7f7ca476b8268746b27cfb4f3a
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2026-01-27 16:56:58 -0800

    update for latest zmk

--- build.fish
+++ build.fish
@@ -2,4 +2,4 @@
 
 [ (uname -r) = Darwin ] && export GNUARMEMB_TOOLCHAIN_PATH=/opt/homebrew || export GNUARMEMB_TOOLCHAIN_PATH=/
 
-for side in left right; ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb west build -p always -s zmk/app -b nice_nano_v2 -- -DCMAKE_PREFIX_PATH=(pwd)/zephyr/share/zephyr-package/cmake -DZMK_CONFIG=(pwd)/config -DSHIELD=cradio_$side && cp build/zephyr/zmk.uf2 cradio_$side.uf2; end
+for side in cradio_left cradio_right settings_reset; ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb west build -p always -s zmk/app -b nice_nano@2.0.0 -- -DCMAKE_PREFIX_PATH=(pwd)/zephyr/share/zephyr-package/cmake -DZMK_CONFIG=(pwd)/config -DSHIELD=$side && cp build/zephyr/zmk.uf2 $side.uf2; end
